3c92c1cd41be2d9d539c9f496c2ca5af7dbc0582
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-03-03 00:17:43 +0400

    task with c#

---
+++ c#/ArrayTasksDavam/Task1/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] array1 = { 1, 2, 3, 4, 5 };
+int[] array2 = { 1, 2, 6, 7, 8 };
+
+for(int i = 0; i < array1.Length; i++)
+{
+    for(int j=0; j < array2.Length; j++)
+    {
+        /*
+         
+        if (array1[i] == array2[j])
+        {
+             Console.WriteLine(array1[i]); //1,2
+        }
+
+        */
+
+        if (array1[i] != array2[j])
+        {
+            Console.WriteLine(array1[i]); 
+        }
+    }
+}
---
+++ c#/ArrayTasksDavam/Task3/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] array = { 1, 2, 3, 2, 5, 3 };
+
+for(int i = 0; i < array.Length; i++)
+{
+    for(int j=i+1; j < array.Length; j++)
+    {
+        if (array[i] == array[j])
+        {
+            Console.Write(array[i]+" ");
+        }
+    }
+}
